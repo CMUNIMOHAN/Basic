bb1e0738df7471cab7d3fc0a2f7ca04a97e93742
Naveen Kumar <naveenk.kamineti@gmail.com>
2017-04-11 17:11:57 +0530

    Second change

---
+++ Hello/src/com/my/main/Basic.java
@@ -0,0 +1,11 @@
+package com.my.main;
+
+public class Basic {
+
+	public static void main(String[] args) {
+     
+		System.out.println("hi ................!");
+		System.out.println("Muni......:)");
+	}
+
+}
